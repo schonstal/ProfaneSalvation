ddf4c81fee6f0bf8e7da576a6d8b5e607eae41b0
Josh Schonstal <josh@schonstal.org>
2019-06-20 10:02:31 -0700

    🎮 Fire projectiles in any direction

--- Projectile.gd
+++ Projectile.gd
@@ -1,35 +1,26 @@
 extends Area2D
 
-export var speed = 1000
-export var direction = -1
-export var vertical = true
+export var velocity = Vector2(0, -1000)
 
 export var damage = 1
-
-const Enemy = preload("res://Enemies/Enemy.gd")
 
 func _ready():
   connect("body_entered", self, "_on_body_enter")
 
 func _physics_process(delta):
-  var pos = position.y if vertical else position.x
+  position += velocity * delta
 
-  pos += speed * delta * direction
-
-  if pos < -100:
+  if position.y < -100:
     self.queue_free()
 
-  if pos > 1000:
+  if position.y > 1200:
     self.queue_free()
 
-  if vertical:
-    position.y = pos
-  else:
-    position.x = pos
+  if position.x < -100:
+    self.queue_free()
 
-func flip():
-  direction = -direction
-  $Sprite.flip_h = !$Sprite.flip_h
+  if position.x > 2020:
+    self.queue_free()
 
 func _on_body_enter(body):
   body.hurt(damage)
