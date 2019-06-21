0bbc4a5cf751aade2dcc78b437dfb5135024e143
Josh Schonstal <josh@schonstal.org>
2019-06-21 15:31:22 -0700

    🎮 Add new pattern to spiral

--- Enemies/Spiral/Spiral.gd
+++ Enemies/Spiral/Spiral.gd
@@ -5,17 +5,30 @@ onready var enemy = $Enemy
 
 export var bullet_count = 24
 export var offset_increment = 0.1
+export var cluster_size = 6
 
 var started = false
 var offset = 0
+var cluster_count = 0
+var spawn = true
 
 export(Resource) var bullet_scene = preload("res://Enemies/Projectiles/PinkProjectile.tscn")
 
 func _ready():
   timer.connect("timeout", self, "_on_Timer_timeout")
+  enemy.connect("died", self, "_on_Enemy_died")
 
 func _on_Timer_timeout():
-  offset += offset_increment * PI * 2
+  if cluster_count >= cluster_size:
+    cluster_count = 0
+    spawn = !spawn
+
+  cluster_count += 1
+
+  if spawn:
+    offset -= offset_increment * PI * 2
+  else:
+    offset += offset_increment * PI * 2
 
   for i in range(0, bullet_count):
     var bullet = bullet_scene.instance()
@@ -25,3 +38,6 @@ func _on_Timer_timeout():
         cos(PI * 2 * i / bullet_count + offset),
         sin(PI * 2 * i / bullet_count + offset)
     ) * 300
+
+func _on_Enemy_died():
+  queue_free()
