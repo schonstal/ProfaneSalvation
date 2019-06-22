5fe905ad87c2b95f838e39b304e3f43f00830149
Josh Schonstal <josh@schonstal.org>
2019-06-22 13:38:02 -0700

    🔧 Add bone background

--- Backgrounds/ParallaxLayer.gd
+++ Backgrounds/ParallaxLayer.gd
@@ -4,7 +4,8 @@ onready var sprite = $Sprite
 export var speed = 2000
 
 func _ready():
-  self.motion_mirroring = sprite.texture.get_size().rotated(sprite.global_rotation)
+  #self.motion_mirroring = Vector2(0, sprite.texture.get_size().rotated(sprite.global_rotation).y)
+  pass
 
 func _process(delta):
   var scroll = Vector2(0, delta * speed)
