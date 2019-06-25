7a30b14a95a9ff9e293ee2a53ce8823079f76f0a
Josh Schonstal <josh@schonstal.org>
2019-06-25 12:02:36 -0700

    🎮 Update basic bat pattern

--- Enemies/BloodBat/BloodBat.gd
+++ Enemies/BloodBat/BloodBat.gd
@@ -1,66 +1,67 @@
 extends Node2D
 
-onready var tween = $Tween
-onready var timer = $Timer
 onready var enemy = $Enemy
 
-export var bullet_count = 12
+var shoot_timer:Timer
 
 var started = false
+var time = PI / 2
+var original_position:Vector2
+var shooting = false
+var pattern = null
 
-var bullet_scene = preload("res://Enemies/Projectiles/Projectile.tscn")
+export var movement = Vector2(100, 100)
+export var velocity = Vector2(0, 200)
+export var move_rate = 1
+export var shoot_time = 0.5
+
+export(Resource) var bullet_pattern = preload("res://BulletPatterns/Lines.tscn")
+
+signal died
 
 func _ready():
-  tween.connect("tween_completed", self, "_on_Tween_tween_completed")
-  timer.connect("timeout", self, "_on_Timer_timeout")
+  shoot_timer = Timer.new()
+  shoot_timer.set_name("ShootTimer")
+  shoot_timer.wait_time = PI / move_rate
+  shoot_timer.one_shot = true
+  shoot_timer.start()
+  add_child(shoot_timer)
+
+  shoot_timer.connect("timeout", self, "_on_ShootTimer_timeout")
+
   enemy.connect("died", self, "_on_Enemy_died")
+  original_position = position
 
-  enemy.position.x = (randi() % 11) * 115 + 315
+func _process(delta):
+  if !shooting:
+    time += delta
 
-  enemy.velocity.y = 0
-  enemy.acceleration.y = 0
+  position.x = original_position.x + sin(time * move_rate) * movement.x
+  position.y = original_position.y + abs(cos(time * move_rate)) * movement.y
 
-  tween.interpolate_property(
-      enemy,
-      "position",
-      enemy.position,
-      Vector2(enemy.position.x, 150),
-      0.5,
-      Tween.TRANS_QUART,
-      Tween.EASE_OUT)
+  if !shooting:
+    original_position += velocity * delta
 
-  tween.start()
+  if enemy.global_position.y > 1120:
+    die()
 
-func _on_Timer_timeout():
-  tween.interpolate_property(
-      enemy,
-      "position",
-      enemy.position,
-      Vector2(enemy.position.x, 1200),
-      2,
-      Tween.TRANS_BACK,
-      Tween.EASE_IN)
-
-  tween.start()
-
-func _on_Tween_tween_completed(object, key):
-  enemy.velocity.x = 0
-  enemy.velocity.y = 0
-
-  if !started:
-    timer.start()
-    started = true
-  else:
-    queue_free()
+func die():
+  emit_signal("died")
+  queue_free()
 
 func _on_Enemy_died():
-  for i in range(0, bullet_count):
-    var bullet = bullet_scene.instance()
-    Game.scene.projectiles.call_deferred("add_child", bullet)
-    bullet.global_position = enemy.global_position
-    bullet.velocity = Vector2(
-        cos(PI * 2 * i / bullet_count),
-        sin(PI * 2 * i / bullet_count)
-    ) * 1000
+  die()
 
-  queue_free()
+func _on_ShootTimer_timeout():
+  shooting = !shooting
+
+  if shooting:
+    if position.y > 0:
+      pattern = bullet_pattern.instance()
+      call_deferred("add_child", pattern)
+
+    shoot_timer.start(shoot_time)
+  else:
+    if pattern != null:
+      pattern.queue_free()
+    shoot_timer.start(PI / move_rate)
