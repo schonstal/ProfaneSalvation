f9d12e3e546790803e4b89cb3382463704eafdff
Josh Schonstal <josh@schonstal.org>
2019-06-26 16:49:01 -0700

    🔧 Make game over menu functional

---
+++ UI/GameOver/Buttons.gd
@@ -0,0 +1,16 @@
+extends VBoxContainer
+
+onready var try_again = $TryAgainButton
+onready var exit = $ExitButton
+
+func _ready():
+  try_again.grab_focus()
+  try_again.connect("pressed", self, "_on_TryAgainButton_pressed")
+
+  exit.connect("pressed", self, "_on_ExitButton_pressed")
+
+func _on_TryAgainButton_pressed():
+  Game.reset()
+
+func _on_ExitButton_pressed():
+  get_tree().change_scene("res://TitleScreen/TitleScreen.tscn")
