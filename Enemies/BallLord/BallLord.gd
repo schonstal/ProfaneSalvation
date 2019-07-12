d9c3663438614d5b02d91e27f265245039d362c9
Josh Schonstal <josh@schonstal.org>
2019-07-12 15:31:17 -0700

    🐛 Fix bug where balls didn't hurt

--- Enemies/BallLord/BallLord.gd
+++ Enemies/BallLord/BallLord.gd
@@ -36,6 +36,14 @@ func _process(delta):
     queue_free()
 
 func _on_Enemy_died():
+  die()
+
+func _on_body_entered(body):
+  if body.has_method("hurt"):
+    body.hurt(1)
+    die()
+
+func die():
   emit_signal("died")
 
   if disable_bullets:
