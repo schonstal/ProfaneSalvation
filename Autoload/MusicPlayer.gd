4a3909488cc285ac8c17b584309ae1d56a571baf
Josh Schonstal <josh@schonstal.org>
2019-09-26 21:17:42 -0700

    🎮 Play music at the beginning and stop after bosses

--- Autoload/MusicPlayer.gd
+++ Autoload/MusicPlayer.gd
@@ -15,6 +15,7 @@ func _ready():
   bus_effect.cutoff_hz = 22000
 
   AudioServer.add_bus_effect(bus_index, bus_effect, effect_index)
+  EventBus.connect("boss_defeated", self, "_on_boss_defeated")
 
 func _process(_delta):
   AudioServer.set_bus_volume_db(bus_index, music_volume)
@@ -26,7 +27,7 @@ func play_file(audio_file):
   current_file = audio_file
 
   self.stream = load(audio_file)
-  # self.play()
+  self.play()
 
 func disable_filter():
   bus_effect.cutoff_hz = 22000
@@ -45,3 +46,6 @@ func fade(start_volume_db, end_volume_db, duration):
       Tween.EASE_IN)
 
   fade_tween.start()
+
+func _on_boss_defeated():
+  self.stop()
