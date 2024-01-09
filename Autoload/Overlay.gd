51135edda0ca1bb652f152cdd74a274c41035d99
Josh Schonstal <josh@schonstal.org>
2024-01-09 14:55:34 -0800

    Temporarily disable transition effect

--- Autoload/Overlay.gd
+++ Autoload/Overlay.gd
@@ -1,23 +1,7 @@
 extends Control
 
 signal fade_complete
-@onready var fade_tween = $Tween
 @onready var rect = $CanvasLayer/ColorRect
 
-func _ready():
-  fade_tween.connect("tween_completed", Callable(self, "_on_FadeTween_tween_completed"))
-
 func fade(start_color, end_color, duration):
-  fade_tween.interpolate_property(
-      rect,
-      "color",
-      start_color,
-      end_color,
-      duration,
-      Tween.TRANS_LINEAR,
-      Tween.EASE_OUT)
-
-  fade_tween.start()
-
-func _on_FadeTween_tween_completed(_object, _key):
   emit_signal("fade_complete")
